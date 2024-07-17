4c7b87989e01f2adac4fd5a63e4ebcb01e32d6ee
codest97 <Madhumitachakraborty12121@gmail.com>
2024-07-17 12:16:07 +0530

    Add files via upload

---
+++ #Python project.py
@@ -0,0 +1,53 @@
+#Python project
+
+import random
+
+x = 'y'
+while x == 'y':
+
+    randNumber = random.randint(1,6)
+    if randNumber==1 :
+        print("\n[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("\n[-----]")
+
+    if randNumber==2 :
+        print("\n[-----]")
+        print("[0    ]")
+        print("[     ]")
+        print("[    0]")
+        print("\n[-----]")
+
+    if randNumber==3 :
+        print("\n[-----]")
+        print("[     ]")
+        print("[0 0 0]")
+        print("[     ]")
+        print("\n[-----]")
+
+    if randNumber==4 :
+        print("\n[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("\n[-----]")
+
+   if randNumber==5 :
+        print("\n[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("\n[-----]")
+
+    if randNumber==6 :
+        print("\n[-----]")
+        print("[0 0 0]")
+        print("[     ]")
+        print("[0 0 0]")
+        print("\n[-----]")
+
+    x = input("\npress y to roll again and n to exit:")
+    print("\n")
+
